41e3e718a64410c96b4b4202693c34de79ddcad7
Tor Hansson Frank <torhanssonfrank@gmail.com>
2018-05-15 14:01:37 +0200

    hittade 3 värden som sakanades för antal fjällripor. La till dem i rådataexcelfilen.

--- 01.script.test.R
+++ 01.script.test.R
@@ -1,1 +1,25 @@
-#testar#####
+library(dplyr)
+library(tidyr)
+
+ripa<-read.csv(file.choose(), header = TRUE, sep = ";")
+head(ripa)
+
+ripa$observation
+sedda<-ripa %>% 
+  select(lya, observation, antal) %>% 
+  filter(observation == "F") %>% 
+  group_by(lya)
+
+View(sedda)
+
+which(is.na(sedda$antal)) #det fattades 3 värden. Jag la till de i rådatan eftersom de fanns nedskrivna på pappren####
+
+
+sedda
+?filter
+?logical
+?complex
+
+sedda  
+  
+  

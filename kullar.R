2f0c6b7801607036de02ef809a91b248a3fc9bf2
Tor Hansson Frank <torhanssonfrank@gmail.com>
2018-05-30 16:51:53 +0200

    antal år med kullar per lya

--- kullar.R
+++ kullar.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(tidyr)
+install.packages("tidyverse")
 
 fjallrav<-read.csv(file.choose(), header = TRUE)
 View(fjallrav)
@@ -9,6 +10,15 @@ minalyor<- fjallrav %>%
   filter(denNr %in% c("FSZZ020", "FSZZ104", "FSZZ042", "FSZZ033", "FSZZ099", "FSZZ062", "FSZZ014", "FSZZ096", "FSZZ076", "FSZZ075", "FSZZ061"))
 
 minalyor %>% 
-  filter(denNr %in% "FSZZ014")
+  filter(denNr %in% "FSZZ020")
 View(minalyor)
 
+minalyor %>% 
+  count(denNr) #ger antal år med kullar per lya. Dock kan det finnas år med två kullar per lya####
+
+årmedkullar<-fjallrav %>% 
+  count(denNr)  #samma för alla lyor####
+
+View(årmedkullar)
+
+which(årmedkullar$n = )

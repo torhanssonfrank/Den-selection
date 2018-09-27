1fe768cf1cee53c0f0acaebf5ec72ed6d22e0ea1
Tor Hansson Frank <torhanssonfrank@gmail.com>
2018-09-27 11:11:50 +0200

    Räknat ut relativt avstånd till rävar som faller inom 2500 m från varje lya

--- rödrävsraster.R
+++ rödrävsraster.R
@@ -16,9 +16,64 @@ buffer$bufferID <- as.character(1:length(buffer$Date))
 red_fox <- readOGR(dsn = "Lyor, kullar, gps-punkter, yta och avstånd/red_fox_2000_2016_sweref/red_fox_2000_2016.shp")
 helagsRaster <- raster("Gnagardata/Lämmelprediktion uppgångsår.tif")
 helagsRaster[!is.na(helagsRaster)] <- 0 # nu har jag ett tomt bakgrundsraster. Pixlarna kanske är för små men kan iofs ta ett medelvärde.
-
 helagsRaster@crs # sweref
 foxBuffer <- gBuffer(red_fox, byid=TRUE, width=1500)
+foxBuffer$fox_ID <- as.character(1:length(foxBuffer$Year))
+foxBuffer$fox_ID
+summary(foxBuffer)
+#' Skriver ut filen igen så att jag har ID för varje obs. Blir kanske enklare att 
+#' hantera den i qgis
+writeOGR(foxBuffer, dsn = "Geodata/buffer runt lyor/rödrävsbuffer2000_2016.shp", layer = "foxBuffer", driver = "ESRI Shapefile")
+
+#' Gör en enkel uträkning på hur många skjutna rödrävar mellan 2000-2016
+#'  som hamnar inom varje lybuffer. Sedan mäter jag avstånd till alla rödrävar
+#'  från alla lyor och tar en subset på alla som är närmare än 2500 meter
+
+lyor <- readOGR(dsn = "Lyor, kullar, gps-punkter, yta och avstånd/lyor helags alla.shp", stringsAsFactors = FALSE)
+summary(lyor) # sweref och projected
+
+#Lägger på en buffer med radie 2500 meter
+lybufferstor <- gBuffer(lyor, byid=TRUE, width=2500)
+summary(lybufferstor)# sweref och projected
+
+overlap<-over(red_fox, lybufferstor)
+
+table(overlap$Namn) #Här är de ihopräknade rävarna.
+
+# Avstånd till alla skjutna rödrävar per lya. Tar bara de som är närmre än 2500 meter
+k<-gDistance(lyor, red_fox, byid = TRUE) # Ger alla avstånd i en matris.
+colnames(k)<- c(lyor$Namn) #Namnen försvinner eftersom outputen är en matris. Namnger igen
+
+k <- as.data.frame(k)
+ks<-stack(k) # stack är en king basfunktion. Den gör om kolumnnamnen till rader. Bra när man jobbar med matriser. De nya kolumnnamnen blir value och ind.
+
+View(ks)
+colnames(ks)<- c("distans", "Namn")
+ks.sub <- ks %>% 
+  group_by(Namn) %>% 
+  filter(distans < 2500)
+
+View(ks.sub)
+
+ks.count <- ks.sub %>%
+  count(Namn)
+
+?count 
+View(ks.count)
+
+ks.sum <- ks.sub %>% 
+  group_by(Namn) %>% 
+  summarise(tot.dist = sum(distans))
+
+(View(ks.sum))
+ks.rel<-ks.sum %>% 
+  left_join(ks.count, by = "Namn")
+
+ks.rel <- ks.rel %>% 
+  mutate(rel.dist = n/tot.dist)
+
+View(ks.rel)
+
 #' En skriven funktion från https://amywhiteheadresearch.wordpress.com/2014/05/01/shp2raster/
 #' för att konvertera shapefiler till raster
 
@@ -55,32 +110,64 @@ shp2raster <- function(shp, mask.raster, label, value, transform = FALSE, proj.f
  
 plot(helagsRaster, axes = F, box = F, legend = F, main = "Helags")
 plot(foxBuffer, add = T)
+summary(fox)
+#' Lämmelrastern är väldigt stor. Den går hela vägen till treriksröset.
+#' Av någon anledning gick den inte beskära i qgis så jag gör det här istället.
+#' Genom att klicka på kartan i qgis med coordinate capture plugin får jag ut koordinaterna
+#' för den västligaste, östligaste, sydligaste och nordligaste punkten som jag vill ha. Sen kan jag
+#' beskära.
+#' 
+west <- 351409.447
+east <-445337.351
+north <-7016939.202
+south <-6955416.424
+e <- as(extent(west, east, south, north), 'SpatialPolygons')
+crs(e) <- "+init=EPSG:3006"
+helagsRaster <- crop(helagsRaster, e)
 
 
+redRaster <- shp2raster(shp = foxBuffer,
+                        mask.raster = helagsRaster, label = "Red fox density in Helags", 
+                        transform = FALSE, value = 1)
+
+
+
+
+
+#' Förslag från https://stat.ethz.ch/pipermail/r-sig-geo/2014-March/020595.html
+#' för att göra en raster av buffer
 #A first cut at an example:
   
   library(sp)
 library(rgeos)
-
-pols <- gBuffer(red_fox, byid=TRUE, width=1500)
-plot(pols, add=TRUE)
+plot(helagsRaster)
+pts<-SpatialPoints(red_fox)
+pols <- gBuffer(pts, byid=TRUE, width=1500)
+proj4string(pols) <- CRS("+init=EPSG:3006")
+plot(pols)
 STR <- gBinarySTRtreeQuery(pols, pols)
 length(STR)
 summary(sapply(STR, length))
-res <- vector(mode="list", length=length(STR))
+res <- list()
 
-for(i in seq(along=STR)) res[[i]] <- gOverlaps(pols[i], pols[STR[[i]]],
+
+for(i in seq(along=STR)){ res[[i]] <- gOverlaps(pols[i], pols[STR[[i]]],
                                                byid=TRUE)
+}
 
+res
 
 summary(sapply(res, sum)-1) # to remove self-counting, i overlaps i
 
+res<-SpatialPolygonsDataFrame(res)
+proj4string(res) <- CRS("+init=EPSG:3006")
+plot(res)
 #So then we have # overlap counts per input polygon - but what are the 
 #output reporting units? Should we actually be counting the number of 
 #polygons that a fine grid of points over box belong to? If we focus on 
 #this, then maybe:
-  
-  GT <- GridTopology(c(0.5, 0.5), c(1, 1), c(1000, 1000))
+?GridTopology
+GT <- GridTopology(c(0.5, 0.5), c(1, 1), c(1000, 1000))
 SG <- SpatialGrid(GT)
 o <- over(SG, pols, returnList=TRUE)
 ct <- sapply(o, length)
